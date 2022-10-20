2e088395ff2d467f74b37b7fcca8e63a8e26f787
DianDian98 <75629000+DianDian98@users.noreply.github.com>
2022-10-21 00:31:25 +0800

    Time: 113 ms (25.40%), Space: 13.9 MB (80.12%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,10 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        # num = [int(num/1000), int(num/100)%10,int(num/10)%10,num%10]
+        symbol = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I']
+        value = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+        ans = ''
+        for i in range(len(symbol)):
+            ans += symbol[i]*int(num/value[i])
+            num = num%value[i]
+        return ans
