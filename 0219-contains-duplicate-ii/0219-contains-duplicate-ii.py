0b0676fcbdd9b99f37e421db60e8bb7e7982aaf6
DianDian98 <75629000+DianDian98@users.noreply.github.com>
2022-10-21 22:35:09 +0800

    Time: 588 ms (99.85%), Space: 26.4 MB (83.82%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,12 +1,15 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         unique = set(nums)
+        # nums全部不同
         if len(nums)==len(unique):  return False
         for n in unique:
             count = nums.count(n)
+            # 只計算含2個以上的元素
             if count>1:
                 first = nums.index(n)
                 while count>1:
+                    # 和下一個重複元素的間隔
                     second = nums[first+1:].index(n)+1
                     if second<=k:
                         return True
