f7d0aed58d75ca1bd4a2f3e239e9db10f746b1f5
DianDian98 <75629000+DianDian98@users.noreply.github.com>
2022-10-23 16:20:27 +0800

    Time: 4201 ms (7.25%), Space: 15.3 MB (84.98%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        num, replace = None, None
+        nums = sorted(nums)
+        for i in range(len(nums)-1):
+            if num==None and nums[i]==nums[i+1]:
+                num = nums[i]
+                print('num', num)
+                if replace!=None: return [num, replace]   
+            if replace==None and i+1 not in nums:
+                replace = i+1
+                print('replace',replace)
+                if num!=None: return [num, replace]
+        
+        return [num,len(nums)]
